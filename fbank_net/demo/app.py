101338b3d78da5247298161fdfe6fb364c388ac3
Thomas Burnett-Taylor <tmfksoft@gmail.com>
2023-07-08 21:02:32 +0100

    feat: Added process and identify endpoints

--- fbank_net/demo/app.py
+++ fbank_net/demo/app.py
@@ -1,9 +1,9 @@
 import os
 import sys
 import logging
-
+import uuid
 import numpy as np
-from flask import Flask, render_template, request, Response
+from flask import Flask, render_template, request, Response, send_file
 
 from .preprocessing import extract_fbanks
 from .predictions import get_embeddings, get_cosine_distance
@@ -48,9 +48,48 @@ def register(username):
     embeddings = get_embeddings(fbanks)
     print('shape of embeddings: {}'.format(embeddings.shape), flush=True)
     mean_embeddings = np.mean(embeddings, axis=0)
+    print(mean_embeddings)
     np.save(DATA_DIR + username + '/embeddings.npy', mean_embeddings)
     return Response('', mimetype='application/json')
 
+# Takes in audio clip and returns an embeddings file
+@app.route('/process', methods=['POST'])
+def process():
+    username = str(uuid.uuid4())
+    filename = _save_file(request, username)
+    fbanks = extract_fbanks(filename)
+    embeddings = get_embeddings(fbanks)
+    print('shape of embeddings: {}'.format(embeddings.shape), flush=True)
+    # Generate and save embeddings to file
+    mean_embeddings = np.mean(embeddings, axis=0)
+    embeddingPath = username + '/embeddings.npy'
+    np.save(DATA_DIR + embeddingPath, mean_embeddings)
+
+    # Return the embedding output
+    return send_file("../" + DATA_DIR + embeddingPath, mimetype='application/octet-stream')
+
+# Takes in an audio clip and embeddings file and returns whether they match
+@app.route('/identify', methods=['POST'])
+def identify():
+    username = str(uuid.uuid4())
+    filename = _save_file(request, username)
+    embeddingsPath = _save_file_embed(request, username)
+
+    fbanks = extract_fbanks(filename)
+    embeddings = get_embeddings(fbanks)
+    stored_embeddings = np.load(embeddingsPath)
+    stored_embeddings = stored_embeddings.reshape((1, -1))
+
+    distances = get_cosine_distance(embeddings, stored_embeddings)
+    print('mean distances', np.mean(distances), flush=True)
+    positives = distances < THRESHOLD
+    positives_mean = np.mean(positives)
+    print('positives mean: {}'.format(positives_mean), flush=True)
+    if positives_mean >= .65:
+        return Response('SUCCESS', mimetype='application/json')
+    else:
+        return Response('FAILURE', mimetype='application/json')
+    
 
 def _save_file(request_, username):
     file = request_.files['file']
@@ -59,5 +98,15 @@ def _save_file(request_, username):
         os.makedirs(dir_)
 
     filename = DATA_DIR + username + '/sample.wav'
+    file.save(filename)
+    return filename
+
+def _save_file_embed(request_, username):
+    file = request_.files['embed']
+    dir_ = DATA_DIR + username
+    if not os.path.exists(dir_):
+        os.makedirs(dir_)
+
+    filename = DATA_DIR + username + '/embeddings.npy'
     file.save(filename)
     return filename
